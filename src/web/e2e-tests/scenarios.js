e4150db76a3df8bc986a6456f1dd7a6991771f2e
Steve Reckamp <sreckamp@gmail.com>
2020-09-03 02:13:53 -0500

    Clean up and rename items

--- src/web/e2e-tests/scenarios.js
+++ src/web/e2e-tests/scenarios.js
@@ -5,20 +5,20 @@
 describe('my app', function() {
 
 
-  it('should automatically redirect to /board when location hash/fragment is empty', function() {
+  it('should automatically redirect to /game when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/board");
+    expect(browser.getLocationAbsUrl()).toMatch("/game");
   });
 
 
   describe('board', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/board');
+      browser.get('index.html#!/game');
     });
 
 
-    it('should render board when user navigates to /board', function() {
+    it('should render game when user navigates to /game', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
