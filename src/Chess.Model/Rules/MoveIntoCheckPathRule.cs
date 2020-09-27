2da88463a7aebe8a8bcf71ba36a9e55fbdcfe9fe
Steve Reckamp <sreckamp@gmail.com>
2020-09-27 14:25:48 -0500

    Don't allow moves in same direction as check

--- src/Chess.Model/Rules/MoveIntoCheckPathRule.cs
+++ src/Chess.Model/Rules/MoveIntoCheckPathRule.cs
@@ -15,16 +15,22 @@ namespace Chess.Model.Rules
         /// <inheritdoc />
         public void Apply(Square start, Path path, ISquareProvider squares)
         {
-            if (start.Piece.Type == PieceType.King)
+            if (start.Piece.Type == PieceType.King &&
+                path.Moves.Any() &&
+                (IsInDirectionOfCheck(start, path.Direction) ||
+                 IsCoveredAgainst(squares.GetSquare(path.Moves.First().To), start.Piece.Color)))
             {
-                var move = path.Moves.FirstOrDefault();
-                if (move != null && squares.GetSquare(move.To).GetMarkers(MarkerType.Cover)
-                    .Cast<SimpleMarker>().Any(marker => marker.Source.Piece.Color != start.Piece.Color))
-                {
-                    return;
-                }
+                // Reject this move
+                return;
             }
+
             m_chain.Apply(start, path, squares);
         }
+
+        private static bool IsInDirectionOfCheck(Square start, Direction direction) => start.GetMarkers(MarkerType.Check)
+            .Any(marker => marker.Direction == direction && marker.Source.Piece.Type != PieceType.Pawn);
+        
+        private static bool IsCoveredAgainst(Square square, Color color) => square.GetMarkers(MarkerType.Cover)
+            .Any(marker => marker.Source.Piece.Color != color);
     }
 }
--- src/Chess.Server/Services/LocalGameProviderService.cs
+++ src/Chess.Server/Services/LocalGameProviderService.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections.Generic;
 using Chess.Model;
 using Chess.Model.Models;
-using Chess.Server.Model;
 using Piece = Chess.Model.Models.Piece;
 
 namespace Chess.Server.Services
 {
     public class LocalGameProviderService : IGameProviderService
     {
-        private static readonly Dictionary<int, Game> m_gameState = new Dictionary<int, Game>
+        private static readonly Dictionary<int, Game> GameState = new Dictionary<int, Game>
         {
             {1, new Game(Version.FourPlayer, new GameBoard(5, 0)
                 {
@@ -25,17 +24,24 @@ namespace Chess.Server.Services
                     [2, 4] = new Piece( PieceType.Queen, Color.Black, Direction.North ),
                     [0, 2] = new Piece( PieceType.Queen, Color.Silver, Direction.West )
                 })
-            }
+            },
+            {3, new Game(Version.TwoPlayer, new GameBoard(5, 0)
+            {
+                [2, 4] = new Piece( PieceType.Queen, Color.Black, Direction.North ),
+                [3, 4] = new Piece( PieceType.Pawn, Color.Black, Direction.North ),
+                [2, 3] = new Piece( PieceType.King, Color.White, Direction.South )
+            })
+        }
         };
 
         public Game GetGame(int id)
         {
-            return m_gameState[id];
+            return GameState[id];
         }
 
         public void StoreGame(int id, Game state)
         {
-            m_gameState[id] = state;
+            GameState[id] = state;
         }
     }
 }
