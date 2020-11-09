c9b475c5de64c8c72acdfa6dc971f0795ac6570a
Steve Reckamp <sreckamp@gmail.com>
2020-11-08 20:28:24 -0600

    Update comments

--- src/Chess.Model/Models/Board/IMarker.cs
+++ src/Chess.Model/Models/Board/IMarker.cs
@@ -4,8 +4,20 @@ namespace Chess.Model.Models.Board
 {
     public interface IMarker
     {
+        /// <summary>
+        /// The type of Marker represented
+        /// </summary>
         MarkerType Type { get; }
+
+        /// <summary>
+        /// The location of the source of the marker
+        /// </summary>
         Point Source { get; }
+
+        /// <summary>
+        /// Duplicates the marker
+        /// </summary>
+        /// <returns></returns>
         IMarker Clone();
     }
 }
