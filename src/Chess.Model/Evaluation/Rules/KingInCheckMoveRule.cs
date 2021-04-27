34f4f792f6214ec1a9e02e82bda70911c6637d2e
Steve Reckamp <sreckamp@gmail.com>
2021-04-27 01:16:04 -0500

    A bit of renaming

---
+++ src/Chess.Model/Evaluation/Rules/KingInCheckMoveRule.cs
@@ -0,0 +1,29 @@
+﻿using Chess.Model.Evaluation.Models;
+
+namespace Chess.Model.Evaluation.Rules
+{
+    /// <summary>
+    /// Limit Squares to only those that prevents the king from being in check
+    /// </summary>
+    public class KingInCheckMoveRule : AbstractPathRule
+    {
+        public KingInCheckMoveRule(IPathRule chain) : base(chain) { }
+
+        public override void Apply(IMarkingsProvider markings, Path path)
+        {
+            base.Apply(new MarkingFilter(tuple =>
+            {
+                var (marks, point) = tuple;
+
+                // If in check:
+                // - Remove all moves that don't fit
+                //   + All King moves
+                //   + If multiple checks, must move king
+                //   + If "None" must take piece or move king
+                //   + Can take piece, move king, or block attack
+
+                return true;
+            }, markings), path);
+        }
+    }
+}
--- src/Chess.Model/Evaluation/Rules/PathRules.cs
+++ src/Chess.Model/Evaluation/Rules/PathRules.cs
@@ -9,17 +9,18 @@
             new FilterPathRule((path => path.AllowTake),
                 new MarkCheckPathRule(
                     new MarkCoverPathRule(
-                        new PinMarkPathRule(NopPathRule.Instance))));
+                        new MarkPinPathRule(NopPathRule.Instance))));
 
         /// <summary>
         /// Rule chain to identify available moves.
         /// </summary>
         public static readonly IPathRule MoveRules =
-            new MoveIntoCheckPathRule(
-                new PawnOpenMovePathRule(
-                    new PawnPromotionMoveRule(
-                        new PinMovePathRule(
-                            new EnPassantTakeMovePathRule(
-                                new MovePathRule(NopPathRule.Instance))))));
+            new KingInCheckMoveRule(
+                new MoveIntoCheckPathRule(
+                    new PawnOpenMovePathRule(
+                        new PawnPromotionMoveRule(
+                            new PinMovePathRule(
+                                new EnPassantTakeMovePathRule(
+                                    new MovePathRule(NopPathRule.Instance)))))));
     }
 }
