3975f590ed0e472d104fcf92403e74950e504269
Steve Reckamp <sreckamp@imo-online.com>
2021-05-08 04:07:52 -0500

    outline to use SignalR for events

---
+++ src/angular/src/app/services/event/event.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+// https://docs.microsoft.com/en-us/aspnet/core/signalr/hubs?view=aspnetcore-5.0
+// https://www.c-sharpcorner.com/article/real-time-angular-11-application-with-signalr-and-net-5/
+// https://medium.com/@dbottiau/simplify-your-realtime-applications-by-bringing-signalr-into-the-ngrx-ecosystem-bc984cf2800c
+  constructor() { }
+}
