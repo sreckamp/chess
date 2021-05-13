9c1284389b1f45bc4e4b330a97c83bc5d6f37ad8
Steve Reckamp <sreckamp@imo-online.com>
2021-05-13 18:48:52 -0500

    Changable background

--- src/angular/src/app/app.component.ts
+++ src/angular/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'chess';
+    private _index = 0;
+    private readonly _backgroundUrls = [
+        'assets/garden.jpg',
+        'assets/generational.jpg',
+        'assets/glass.jpg',
+        'assets/modern.jpg',
+        'assets/ocean.jpg',
+        'assets/red-glass.jpg',
+        'assets/sunrise.jpg',
+        'assets/wooden.png',
+    ];
+
+    backgroundUrl = 'assets/red-glass.jpg';
+
+    constructor() {
+        this.changeBackground();
+    }
+
+    changeBackground(): void {
+        this.backgroundUrl = this._backgroundUrls[this._index++];
+        if (this._index >= this._backgroundUrls.length){
+            this._index = 0;
+        }
+    }
 }
