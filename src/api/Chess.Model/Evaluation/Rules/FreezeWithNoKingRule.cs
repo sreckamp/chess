7631b72833dcc16d3a54b17c13716f34178a8347
Steve Reckamp <sreckamp@gmail.com>
2021-05-13 00:46:50 -0500

    Full screen background Don't take over pieces

---
+++ src/api/Chess.Model/Evaluation/Rules/FreezeWithNoKingRule.cs
@@ -0,0 +1,23 @@
+﻿using Chess.Model.Evaluation.Models;
+
+namespace Chess.Model.Evaluation.Rules
+{
+    public class FreezeWithNoKingRule : AbstractPathRule
+    {
+        public FreezeWithNoKingRule(IPathRule chain) : base(chain)
+        {
+        }
+
+        public bool Enabled { get; set; }
+
+        public override void Apply(IMarkingsProvider markings, Path path)
+        {
+            if (Enabled && !markings.KingLocations.ContainsKey(path.Piece.Color))
+            {
+                System.Console.WriteLine($"{Enabled}::{path.Piece}");
+                return;
+            }
+            base.Apply(markings, path);
+        }
+    }
+}
